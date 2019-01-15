ff5c5d3b8ae2fb17358c53adff33b5c5524af9ae
Lucas Francisco de Araujo <lucas1999araujo@gmail.com>
2019-01-14 22:51:16 -0200

    checkpoint

---
+++ Testando3D/Assets/SetGravityByTime.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.Helpers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGravityByTime : MonoBehaviour
+{
+    public float time;
+    CountDown cd;
+    Rigidbody r;
+
+    void Start()
+    {
+        cd = new CountDown(time);
+        r = GetComponent<Rigidbody>();
+        r.isKinematic = true;
+        cd.StartToCount();
+    }
+
+    void Update()
+    {
+        if (r.isKinematic)
+        {
+            cd.DecreaseTime();
+            if (cd.ReturnedToZero)
+                r.isKinematic = false;
+        }
+    }
+}
